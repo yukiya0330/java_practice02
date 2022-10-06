099e9b6a0ebc3f691a24d7d3b61f2364ece4b31d
yukiya <hosiyky33077@gmail.com>
2022-10-06 11:20:13 +0900

    ~58

---
+++ java_practise02/src/java_practise02/P_1_2.java
@@ -0,0 +1,15 @@
+package java_practise02;
+
+public class P_1_2 {
+
+	public static void main(String[] args) {
+
+		int a = 3;
+		int b = 5;
+		int c = 3 * 5;
+
+		System.out.println("縦幅 " + a + " 横幅 " + b + " の長方形の面積は、" + c);
+
+	}
+
+}
---
+++ java_practise02/src/java_practise02/P_1_3.java
@@ -0,0 +1,17 @@
+package java_practise02;
+
+public class P_1_3 {
+
+	public static void main(String args[]) {
+
+		boolean a = true;
+		String b = "駆";
+		double c = 3.14;
+		long d = 31415926583979L;
+		String e = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+
+		System.out.println(a + b + c + d + e);
+
+	}
+
+}
