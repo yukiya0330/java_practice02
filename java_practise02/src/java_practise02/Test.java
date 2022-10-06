2e0582e942222180c5f366e825698b64a24ba2a9
yukiya <hosiyky33077@gmail.com>
2022-10-06 21:50:31 +0900

    test

---
+++ java_practise02/src/java_practise02/Test.java
@@ -0,0 +1,14 @@
+package java_practise02;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		int[] scores = {20, 30, 40, 50, 80};
+
+		for(int value : scores) {
+			System.out.println(value);
+		}
+	}
+
+}
