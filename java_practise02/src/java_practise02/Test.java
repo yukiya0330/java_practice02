948de9518668338cbb6bc72ea52203b40273f9ef
yukiya <hosiyky33077@gmail.com>
2022-10-08 22:09:11 +0900

    test

--- java_practise02/src/java_practise02/Test.java
+++ java_practise02/src/java_practise02/Test.java
@@ -2,13 +2,13 @@ package java_practise02;
 
 public class Test {
 
-//	  public static int add(int x, int y) {
-//		    int ans = x + y;
-//		    return ans;
-//		  }
-//		  public static void main(String[] args) {
-//		    int ans = add(100, 10);
-//		    System.out.println("100 + 10 = " + ans);
-//		  }
+	  public static int add(int x, int y) {
+		    int ans = x + y;
+		    return ans;
+		  }
+		  public static void main(String[] args) {
+		    int ans = add(100, 10);
+		    System.out.println("100 + 10 = " + ans);
+		  }
 
 }
