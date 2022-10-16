392656ca8eeea400a1f706a75d4a907e9d00e4f6
yukiya <hosiyky33077@gmail.com>
2022-10-16 23:34:12 +0900

    test

---
+++ java_practise02/src/java_practise02/S_03.java
@@ -0,0 +1,21 @@
+package java_practise02;
+
+public class S_03 {
+
+	public static void main(String[] args) {
+
+		int num = 1;
+		switch(num) {
+		case 1:
+		case 2:
+		case 3:
+			System.out.println("A");
+		case 4:
+			System.out.println("B");
+		default:
+			System.out.println("C");
+		}
+
+	}
+
+}
