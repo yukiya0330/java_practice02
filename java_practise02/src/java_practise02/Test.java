e0db3c9f0f748144edb11b718992afbd63603253
yukiya <hosiyky33077@gmail.com>
2022-10-17 23:19:10 +0900

    test

--- java_practise02/src/java_practise02/Test.java
+++ java_practise02/src/java_practise02/Test.java
@@ -2,13 +2,20 @@ package java_practise02;
 
 public class Test {
 
-	  public static int add(int x, int y) {
-		    int ans = x + y;
-		    return ans;
-		  }
-		  public static void main(String[] args) {
-		    int ans = add(100, 10);
-		    System.out.println("100 + 10 = " + ans);
-		  }
+	public static void main(String[] args) {
+
+		int num = 3;
+		switch(num) {
+		case 1:
+		case 2:
+		case 3:
+			System.out.println("A");
+		case 4:
+			System.out.println("B");
+		default:
+			System.out.println("C");
+		}
+
+	}
 
 }
