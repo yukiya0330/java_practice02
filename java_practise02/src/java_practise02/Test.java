ff7bb9c05f4ba6f4378f594feb52cacdcfa2ec75
yukiya <hosiyky33077@gmail.com>
2022-10-19 22:09:11 +0900

    test

--- java_practise02/src/java_practise02/Test.java
+++ java_practise02/src/java_practise02/Test.java
@@ -4,17 +4,11 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		int num = 3;
-		switch(num) {
-		case 1:
-		case 2:
-		case 3:
-			System.out.println("A");
-		case 4:
-			System.out.println("B");
-		default:
-			System.out.println("C");
-		}
+		String a = "sample";
+		String b = "sample";
+		System.out.print(a == b);
+		System.out.print(", ");
+		System.out.println(a.equals(b));
 
 	}
 
