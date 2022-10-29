4a4a9085ea64b98ef22b1f6183034d43230066bd
yukiya <hosiyky33077@gmail.com>
2022-10-29 21:38:41 +0900

    pactise

---
+++ java_practise02/src/java_practise02/BuzzFizz.java
@@ -0,0 +1,19 @@
+package java_practise02;
+
+public class BuzzFizz {
+
+	public static void main(String[] args) {
+		for(int num = 1; num <= 100; num++) {
+			if(num % (3 * 5) == 0) {
+				System.out.println("FizzBuzz");
+			}else if(num % 3 == 0) {
+				System.out.println("Fizz");
+			} else if(num % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(num);
+			}
+		}
+	}
+
+}
