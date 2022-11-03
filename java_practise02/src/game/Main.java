3899ec27ccfb84a84313296b2fef66b8b907cfa1
yukiya <hosiyky33077@gmail.com>
2022-11-04 08:41:04 +0900

    10章まで

--- java_practise02/src/game/Main.java
+++ java_practise02/src/game/Main.java
@@ -3,27 +3,30 @@ package game;
 public class Main {
 
 	public static void main(String[] args) {
-		Sword s = new Sword();
-		s.name = "炎の剣";
-		s.damage = 10;
+//		Sword s = new Sword();
+//		s.name = "炎の剣";
+//		s.damage = 10;
+//
+//		Hero h = new Hero("ミナト");
+//		h.sword = s;
+//		System.out.println("勇者" + h.name + "を生み出しました！");
+//		System.out.println("現在の武器は" + h.sword.name);
+//
+//		Matango m1 = new Matango();
+//		m1.hp = 50;
+//		m1.suffix = 'A';
+//
+//		Matango m2 = new Matango();
+//		m2.hp = 48;
+//		m2.suffix = 'B';
+//
+//		h.slip();
+//		m1.run();
+//		m2.run();
+//		h.run();
 
-		Hero h = new Hero("ミナト");
-		h.sword = s;
-		System.out.println("勇者" + h.name + "を生み出しました！");
-		System.out.println("現在の武器は" + h.sword.name);
-
-		Matango m1 = new Matango();
-		m1.hp = 50;
-		m1.suffix = 'A';
-
-		Matango m2 = new Matango();
-		m2.hp = 48;
-		m2.suffix = 'B';
-
-		h.slip();
-		m1.run();
-		m2.run();
-		h.run();
+		SuperHero sh = new SuperHero();
+		sh.run();
 	}
 
 }
